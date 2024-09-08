660636059c7204ab37b7b07a020719e6e60f62a6
sarahlesliegraham <sarah.l.grahamm@gmail.com>
2024-09-08 15:50:59 -0500

    Update main.dart
    Color change when + button pressed

--- test_drive/lib/main.dart
+++ test_drive/lib/main.dart
@@ -118,6 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
+        backgroundColor: Colors.blue,
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
